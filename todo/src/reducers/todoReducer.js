2f973609cbe1a2a29efcd73bdcca94ad2e16d0bf
imxande <alerodriguezlarosa@gmail.com>
2019-12-10 16:07:02 -0500

    created reducer, initial state and a todo component

---
+++ todo/src/reducers/todoReducer.js
@@ -0,0 +1,12 @@
+export const reducer = (state, action) => {
+    switch(action.type){
+        default:
+            return state;
+    }
+}
+
+export const initialState = {
+    item: 'Learn about reducers',
+    completed: false,
+    id: 3892987589
+}
